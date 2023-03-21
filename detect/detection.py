21c2548689df8ae4a047d28f19e0c5f51de408c3
CYC1110 <vschan1110@gmail.com>
2023-03-21 11:09:26 +0900

    23.03.21 update v0.5

--- detect/detection.py
+++ detect/detection.py
@@ -20,7 +20,7 @@ class Detection():
 
         vfile = cv2.VideoCapture(video_path)
 
-        codec =cv2.VideoWriter_fourcc(*'XVID')
+        codec =cv2.VideoWriter_fourcc(*'mp4v')
 
         vid_size = (round(vfile.get(cv2.CAP_PROP_FRAME_WIDTH)),round(vfile.get(cv2.CAP_PROP_FRAME_HEIGHT)))
         vid_fps = vfile.get(cv2.CAP_PROP_FPS)
@@ -30,15 +30,27 @@ class Detection():
         frame_cnt = int(vfile.get(cv2.CAP_PROP_FRAME_COUNT))
         print('총 Frame 갯수:', frame_cnt)
 
-        while True:
-            vret, img = vfile.read()
-            if not vret:
-                print('더 이상 처리할 frame이 없습니다.')
-                break
-            img = self.model(img)
-            result_img = img.render()[0]
-            vid_writer.write(result_img)
+        with tqdm(total=frame_cnt, desc="Processing", unit="frame") as pbar:
+            while vfile.isOpened(): # 비디오캡쳐 객체가 열렸는지 확인
+                ret, frame = vfile.read() 
+                # 재생되는 비디오의 한 프레임씩 읽음. 프레임을 읽었다면 ret : True, 읽은 프레임 : frame
+                if not ret: # 새로운 프레임을 못 받아 왔을때 break (ret이 False면 중지)
+                    break
+                results = self.model(frame)
+                result_frame = results.render()[0]
+                vid_writer.write(result_frame) # 프레임 저장
+                pbar.update(1)
+
+        # while True:
+        #     vret, img = vfile.read()
+        #     if not vret:
+        #         print('더 이상 처리할 frame이 없습니다.')
+        #         break
+        #     img = self.model(img)
+        #     result_img = img.render()[0]
+        #     vid_writer.write(result_img)
         
         vid_writer.release()
         vfile.release()
+        cv2.destroyAllWindows()
         
